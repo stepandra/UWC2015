22bfae7623925af82629705fdcb2dbf10696c223
Stefan Mytchyk <stepandra94@gmail.com>
2015-03-25 02:44:30 +0200

    Created setup.py
    Config project dependencies.

---
+++ client/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="Volunteers",
+    version="0.1",
+    install_requires=[
+        "Flask",
+        "flask-bcrypt",
+        "flask-login",
+        "peewee",
+        "Flask-WTF",
+    ]
+)
