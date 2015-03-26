b4e8bc70213ad4c0b59329d9559f9f20fc9e2fca
Stefan Mytchyk <stepandra94@gmail.com>
2015-03-26 23:39:18 +0200

    Deleted unused files. Improved setup file.
    Added dependencies to setup.py, deleted models.py and api/json folder.
    Removed db init code to __init__.py(server/api). Improved __init__.py
    at server directory.

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, g
+from flask.ext import restful
+from flask.ext.login import current_user
+import server.api.user_model as models
+from peewee import *
 
+db = SqliteDatabase('database.db')
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../client/')
 app._static_folder = "../client/"
 app.secret_key = 'auoesh.bouoastuh.45,uoausoehuosth3ououea.auoub!'
 
+
+api_call = restful.Api(app)
+
+@app.before_request
+def before_request():
+    g.db = models.db
+    g.db.connect()
+    g.user = current_user
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 import server.views
 
